b71167ece52c40f3fd3e38e021f9fa4a09524294
UnwieldySalt <144175961+UnwieldySalt@users.noreply.github.com>
2023-12-02 20:05:46 -0500

    Add files via upload

---
+++ FinalProject/LEGO/JAVASCRIPT/globalHeaderFooter.js
@@ -0,0 +1,35 @@
+
+
+function changeToProductPage()
+{
+    // similar behavior as an HTTP redirect
+    window.location.replace("../HTML");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "../HTML/productPage.html";
+}; 
+function changeToHomePage()
+{
+    // similar behavior as an HTTP redirect
+    window.location.replace("../HTML");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "../HTML/index.html";
+}; 
+function changeToAboutPage()
+{
+    // similar behavior as an HTTP redirect
+    window.location.replace("../HTML");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "../HTML/aboutCompany.html";
+}; 
+function changeToContactPage()
+{
+    // similar behavior as an HTTP redirect
+    window.location.replace("../HTML");
+    //or
+    // similar behavior as clicking on a link
+    window.location.href = "../HTML/contactPage.html";
+}; 
+ 
